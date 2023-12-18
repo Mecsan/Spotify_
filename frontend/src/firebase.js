ea5f57174e9e3c937b32827bd823d080e6f5c07c
Mecsan <mecwansanket333@gmail.com>
2023-12-18 12:29:30 +0530

    firebase bug fix

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
-  messagingSenderId: process.env.MESSAGESENDERID,
-  appId: process.env.APPID,
-  measurementId: process.env.MESSUREMENTID
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGESENDERID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MESSUREMENTID
 }
-
+console.log(firebaseConfig)
 const app = initializeApp(firebaseConfig);
 export default app;
